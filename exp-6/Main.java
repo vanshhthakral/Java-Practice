8f627bff43e69e7259c84647d428c8418b85e9a7
vanshhthakral <vanshnrw@gmail.com>
2025-03-28 10:06:35 +0530

    Experiment -6

---
+++ exp-6/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object of the final class Logger
+        Logger logger = new Logger();
+        logger.logMessage("This is a sample log message.");
+    }
+}
